fcfd201e9147a2d1bbde6335ce40c02782338abb
Francisco Berroeta <fsberroeta@gmail.com>
2024-11-14 12:54:43 -0300

    feat: implement toggle border effect on image click and update HTML structure

--- 2-conditionals/assets/js/main.js
+++ 2-conditionals/assets/js/main.js
@@ -1,1 +1,9 @@
 console.log('main.js loaded');
+
+// Get the picture element by id
+const oliPicture = document.getElementById('oli-picture');
+
+// Add a click event listener to the picture element that toggles the class 'bordered'
+oliPicture.addEventListener('click', function () {
+  oliPicture.classList.toggle('bordered');
+});
