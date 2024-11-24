e56913b228fd4e86d3f0e6ef2be1041f7fe19a4d
berrodev <fsberroeta @gmail.com>
2024-11-24 19:47:22 -0300

    refactor: simplify event listener callbacks for background color change

--- 3-functions/4_colores/assets/js/script.js
+++ 3-functions/4_colores/assets/js/script.js
@@ -3,24 +3,16 @@ const div2 = document.getElementById('div2');
 const div3 = document.getElementById('div3');
 const div4 = document.getElementById('div4');
 
-const changeBGToBlack = (element) => {
-  element.style.backgroundColor = 'black';
+const changeBGToBlack = (event) => {
+  event.target.style.backgroundColor = 'black';
 };
 
 //add event listener
 
-div1.addEventListener('click', () => {
-  changeBGToBlack(div1);
-});
+div1.addEventListener('click', changeBGToBlack);
 
-div2.addEventListener('click', () => {
-  changeBGToBlack(div2);
-});
+div2.addEventListener('click', changeBGToBlack);
 
-div3.addEventListener('click', () => {
-  changeBGToBlack(div3);
-});
+div3.addEventListener('click', changeBGToBlack);
 
-div4.addEventListener('click', () => {
-  changeBGToBlack(div4);
-});
+div4.addEventListener('click', changeBGToBlack);
