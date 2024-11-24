52e8e49dc4b6c36e2691a37ccca94131b770cdd4
berrodev <fsberroeta @gmail.com>
2024-11-24 19:47:53 -0300

    feat: add event listeners to div elements for background color change

--- 3-functions/4_colores/assets/js/script.js
+++ 3-functions/4_colores/assets/js/script.js
@@ -1,13 +1,15 @@
+// Get the div elements
 const div1 = document.getElementById('div1');
 const div2 = document.getElementById('div2');
 const div3 = document.getElementById('div3');
 const div4 = document.getElementById('div4');
 
+// Create a function that changes the background color of the div to black
 const changeBGToBlack = (event) => {
   event.target.style.backgroundColor = 'black';
 };
 
-//add event listener
+// Add event listeners to the divs
 
 div1.addEventListener('click', changeBGToBlack);
 
